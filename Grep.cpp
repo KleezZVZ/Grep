6a0cbfd86a95c4dca15e0dd74a46d51a94da95ce
Mario Márquez <mariomarquez.0116@gmail.com>
2023-10-08 10:48:06 -0400

    Feat: Codigo de lector de archivos agregado.

--- Grep.cpp
+++ Grep.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-int main(char* args[]){
-    cout<<args[1]<<" "<<args[2]<<endl;
+int main(int argc, char** argv){
+   if(argc>3){
+    cout<<"Solo puede ingresar dos [2] argumentos";
     return 0;
-};
+   }else{
+        ifstream in_file (argv[2], fstream::in|fstream::out|fstream::binary);
+        if(!in_file.is_open()){
+            cout<<"Archivo no encontrado"<<endl;
+            return 0;
+        }else{
+            cout<<"Archivo encontrado"<<endl;
+        }
+   }
+   return 0;
+}
