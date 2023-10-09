b588cc852d05eade5c886128b3f1903658602d57
Mario Márquez <mariomarquez.0116@gmail.com>
2023-10-09 17:35:39 -0400

    Feat: Capacidad de poder comparar y colorear las coincidencias

--- Grep.cpp
+++ Grep.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
+#include <cctype>
+#include <Windows.h>
+#include <stdlib.h>
 using namespace std;
-string caracteres;
+char caracteres[100];
+string caracteres_impreso;
+int color=2, coincidencia=0, salto_linea=0;
 int main(int argc, char** argv){
    if(argc>3){
     cout<<"Solo puede ingresar dos [2] argumentos";
@@ -12,10 +18,26 @@ int main(int argc, char** argv){
             cout<<"Archivo no encontrado"<<endl;
             return 0;
         }else{
-            while(in_file.good()){
+            do{
                 in_file>>caracteres;
-                cout<<caracteres<<endl;
-            }
+                caracteres_impreso=caracteres;
+                 for(int i=0; i<strlen(caracteres); i++){
+                    caracteres[i]= tolower(caracteres[i]);
+                }for(int i=0; i<strlen(argv[1]); i++){
+                    argv[1][i]= tolower(argv[1][i]);
+                }if(strcmp(argv[1], caracteres)==0){
+                    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+                    cout<<caracteres_impreso<<endl;
+                    coincidencia++;
+                    salto_linea++;
+                }else{
+                    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),7);
+                    cout<<caracteres_impreso<<endl;
+                    salto_linea++;
+                }
+            }while(in_file.good());
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),7);
+            cout<<coincidencia<<" coincidencias en "<<salto_linea<<" lineas";
         }
    }
    return 0;
